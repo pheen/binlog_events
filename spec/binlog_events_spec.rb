2bbb609860867c566a4476487c76029c38c6619d
Joel Korpela <syright@gmail.com>
2022-10-10 22:06:57 -0700

    segfault when connecting to Mysql under Rutie, i'll need to a new plan...

--- spec/binlog_events_spec.rb
+++ spec/binlog_events_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe BinlogEvents do
   it "has a version number" do
     expect(BinlogEvents::VERSION).not_to be nil
   end
+
+  it "does things" do
+    url = "mysql://root:root@127.0.0.1:3306/themis_development_1"
+
+    # BinlogEventsInterface.listen(url) do |table_name, column_name, value|
+    BinlogEventsInterface.listen(url) do |change|
+      puts change
+    end
+  end
 end
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+use std::borrow::Borrow;
+use std::collections::HashMap;
+use std::io::Read;
+use std::ops::Index;
+use std::str::from_utf8;
+
+use mysql::*;
+use mysql::binlog::events::Event;
+use mysql::binlog::events::EventData;
+use mysql::binlog::events::RowsEventData;
+use mysql::binlog::events::TableMapEvent;
+use mysql::binlog::events::UpdateRowsEvent;
+use mysql::prelude::*;
+use mysql::BinlogStream;
+use mysql::binlog::BinlogVersion;
+use mysql::BinlogRequest;
+
 #[macro_use]
 extern crate rutie;
 
@@ -8,19 +25,49 @@ class!(BinlogEvents);
 methods!(
     BinlogEvents,
     _rtself,
+    fn pub_listen(mysql_url: RString) -> RString {
+        // if !VM::is_block_given() {
+        //     //     let argument = ruby_string.to_any_object();
+        //     //     let result = VM::yield_object(argument);
+        //     // } else {
+        //     VM::raise(
+        //         Class::from_existing("LocalJumpError"),
+        //         "no block given (yield)",
+        //     );
+        //     unreachable!();
+        // }
 
-    fn pub_reverse(input: RString) -> RString {
-        let ruby_string = input.
-          map_err(|e| VM::raise_ex(e) ).
-          unwrap();
+        let ruby_string = mysql_url.map_err(|e| VM::raise_ex(e)).unwrap();
+        // let url = ruby_string.to_str();
+        let url = "mysql://root:root@127.0.0.1:3306/themis_development_1";
+        // let pool = Pool::new(url).unwrap();
+        match Pool::new(url) {
+            Ok(_) => {},
+            Err(err) => { println!("{:#?}", err); }
+        }
+        // let mut conn = pool.get_conn().unwrap();
 
-        RString::new_utf8(
-          &ruby_string.
-          to_string().
-          chars().
-          rev().
-          collect::<String>()
-        )
+        // for result in conn.query_iter("SELECT TABLE_NAME, COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS ORDER BY ORDINAL_POSITION").unwrap() {
+        //     let result = result.unwrap();
+        //     let table_name_result = result.index(0);
+
+        //     if let Value::Bytes(table_name) = table_name_result {
+        //         let name = from_utf8(table_name).unwrap();
+        //         let name = RString::new_utf8(name);
+        //         VM::yield_object(name);
+        //         // println!("{:#?}", from_utf8(table_name).unwrap());
+        //     }
+        // }
+
+        RString::new_utf8("Done")
+
+        // RString::new_utf8(
+        //     &ruby_string.
+        //     to_string().
+        //     chars().
+        //     rev().
+        //     collect::<String>()
+        // )
     }
 );
 
@@ -28,6 +75,6 @@ methods!(
 #[no_mangle]
 pub extern "C" fn Init_binlog_events() {
     Class::new("BinlogEventsInterface", None).define(|klass| {
-        klass.def_self("reverse", pub_reverse);
+        klass.def_self("listen", pub_listen);
     });
 }
