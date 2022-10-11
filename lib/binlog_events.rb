dee4fc72a5a32448c1d50a4e02c699c9e498f7f5
Joel Korpela <syright@gmail.com>
2022-10-10 18:34:37 -0700

    initial gem

---
+++ lib/binlog_events.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "binlog_events/version"
+
+module BinlogEvents
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/binlog_events/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module BinlogEvents
+  VERSION = "0.1.0"
+end
